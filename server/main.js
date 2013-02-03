e225310fea1066894565fcc539a92347bd1ae1b9
Sate <markc@Marks-MacBook-Air.local>
2013-02-03 02:16:42 -0800

    removed most books for testing

--- server/main.js
+++ server/main.js
@@ -1,24 +1,20 @@
 Meteor.absoluteUrl('home', {rootUrl: 'http://bookcrumb-marx.dotcloud.com'});
 
-var url = "https://api.pearson.com/penguin/classics/v1/books?"
+var url = "https://api.pearson.com/penguin/classics/v1/books/cadk020qnEqNcx?"
       + "apikey=d5df23ffde529d41f3418a80ec6eca25&limit=100"
   Meteor.startup(function () {
-    Meteor.setTimeout(function () {
-      //no text...
-      Books.remove({
-        title: "The Private Memoirs and Confessions of a Justified Sinner"
-      })
-    }, 1000 * 60 * 5)
 
     if (! Books.find().fetch().length)
-      console.log('populating books collection') || 
       Meteor.http.get(url, function (err, result) {
-      result.data.books.forEach(function (book, i) { 
+      var temp = [];
+      temp.push(result);
+      temp.forEach(function (book, i) { 
+        book.title="Moby-Dick";
         book.text = [];
         book.notes = [];
         var id = Books.insert(book)
         Meteor.setTimeout(function ()  {
-          Meteor.http.get(book.url, function (err, result) {
+          Meteor.http.get(url, function (err, result) {
             result.data.book.articles.forEach(function (article, i) {
               Meteor.setTimeout(function ()  {
                 Meteor.http.get(article.url, function (err, result) {
